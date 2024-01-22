c756c05b6aa0cd002e4c41d8ef4a8441ca7e7eeb
Erfan <Erfansharafi60@gmail.com>
2024-01-22 08:37:13 +0330

    config tailwind-cli

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+//! هسته اصلی تیلویند ما که هر کانفیگی باید اینجا ست بشه
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
